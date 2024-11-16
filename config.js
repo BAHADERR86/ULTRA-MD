99aecc5a30ab243538cacea242dbf865e12914d0
BAHADERR86 <bahaderbhatti33@gmail.com>
2024-11-16 08:35:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '923704010810
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS = 'your number';  // put your number here
+const ownervb = process.env.OWNERS = 'your number';  //923704010810
 
 const ownerlist = ownervb.split(';');
 
